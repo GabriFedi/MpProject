fe1e494639971a93528b0070ec1761754818c48e
Gabriele Fedi <gabriele.fedi@elbuild.it>
2018-11-24 19:02:24 +0100

    aggiunto pattern Composite opponent

---
+++ src/mpproject/Player.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mpproject;
+
+/**
+ *
+ * @author work
+ */
+public class Player implements Opponent{
+    private final String name;
+    private double strenght;
+
+    public Player(String name, double strenght) {
+        this.name = name;
+        this.strenght = strenght;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public double getStrenght() {
+        return strenght;
+    }
+
+    public void setStrenght(double strenght) {
+        this.strenght = strenght;
+    }
+    
+    
+    
+}
---
+++ src/mpproject/Team.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mpproject;
+
+import java.util.LinkedList;
+import java.util.stream.Stream;
+
+/**
+ *
+ * @author work
+ */
+public class Team implements Opponent{
+    private final String name;
+    private LinkedList<Opponent> players;
+
+    public Team(String name) {
+        this.name = name;
+        players = new LinkedList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+    
+    public void addPlayer(Opponent o){
+        players.add(o);
+    }
+    
+    public Stream getPlayers(){
+        return players.stream();
+    }
+    
+    @Override
+    public double getStrenght(){
+        return getPlayers().mapToDouble(Opponent::getStrenght).sum();
+    }
+    
+    
+}
