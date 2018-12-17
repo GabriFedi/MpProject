168668ed9d900b4bd6e6125feb0bdf1bf6ced6b6
Gabriele Fedi <gabriele.fedi@elbuild.it>
2018-12-17 16:41:26 +0100

    visitor

--- src/visitor/SetUpCheckout.java
+++ src/visitor/SetUpCheckout.java
@@ -5,7 +5,9 @@
  */
 package visitor;
 
+import java.util.Vector;
 import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
 import mpproject.Cart;
 
 /**
@@ -14,16 +16,26 @@ import mpproject.Cart;
  */
 public class SetUpCheckout implements MyVisitor<Cart>{
 
-    private javax.swing.JTable jTable;
+    private DefaultTableModel model;
+    
 
     public SetUpCheckout(JTable jTable) {
-        this.jTable = jTable;
+        model = (DefaultTableModel) jTable.getModel();
+
+        
     }
     
     
     @Override
     public void visit(Cart obj) {
+        Vector row = new Vector();
+   
+        model.addRow(row);
         obj.getItems().forEach(e -> {
+            row.add(e.getImage());
+            row.add(e.getPrice());
+            row.add(e.getQty());
+            row.removeAllElements();
            
         });
     }
